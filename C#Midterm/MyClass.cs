6d9f2c16207425d31bed5a1201f17afb561f0c3c
leivl <leivl@DESKTOP-G6K9FF2>
2024-10-24 17:39:33 +0800

    Add project files.

---
+++ C#Midterm/MyClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace C_Midterm
+{
+    internal class MyClass
+    {
+        private int rows;
+
+        public int NumberOfRow
+        {
+            get { return rows; }
+            set { rows = value; }
+        }
+
+        public string PrintPyramid(int userRows)
+        {
+            string Pyramid = string.Empty;
+            rows = 0;
+            for (NumberOfRow = 0; NumberOfRow <= userRows; NumberOfRow++)
+            {
+                for (int asterisk = 1; asterisk <= userRows - NumberOfRow; asterisk++)
+                {
+                    Pyramid += " ";
+                }
+                for (int asterisk = 1; asterisk <= 2 * NumberOfRow - 1; asterisk++)
+                {
+                    Pyramid += "*";
+                }
+                Pyramid += "\n";
+            }
+            return Pyramid;
+        }
+
+        public string PrintRightTriangle(int userRows)
+        {
+            string Pyramid = string.Empty;
+            rows = 0;
+            for (NumberOfRow = 0; NumberOfRow <= userRows; NumberOfRow++)
+            {
+                for (int asterisk = 1; asterisk <= 2 * NumberOfRow - 1; asterisk++)
+                {
+                    Pyramid += "*";
+                }
+                Pyramid += "\n";
+            }
+            return Pyramid;
+        }
+
+    }
+
+}
---
+++ C#Midterm/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Midterm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass triangles = new MyClass();
+            Console.Write("Input number of rows: ");
+            int userRows = Int32.Parse(Console.ReadLine());
+
+            string pyramid = triangles.PrintPyramid(userRows);
+            string rightTriangle = triangles.PrintRightTriangle(userRows);
+
+            Console.WriteLine(pyramid);
+            Console.WriteLine(rightTriangle);
+
+            Console.ReadLine();
+
+        }
+    }
+}
